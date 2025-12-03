0690628c06d1719063ec0d05a374f53cc4cc97b1
Mateusz Basiaga <basmateusz@wp.pl>
2025-12-03 14:59:09 +0100

    feat: add fully optional Jira support

--- cz_conventional_commmits_jira_optional.py
+++ cz_conventional_commmits_jira_optional.py
@@ -1,7 +1,9 @@
 import os
+import re
 from typing import TypedDict
 
 from commitizen import defaults
+from commitizen.cz import exceptions
 from commitizen.cz.base import BaseCommitizen
 from commitizen.cz.utils import multiple_line_breaker, required_validator
 from commitizen.question import CzQuestion
@@ -17,7 +19,20 @@ def _parse_subject(text: str) -> str:
     return required_validator(text.strip(".").strip(), msg="Subject is required.")
 
 
+def _parse_jira_issues(text: str) -> str:
+    issues = " ".join(text.strip().split())
+
+    if not all(re.match(r"^[A-Z]{2,}-\d+$", issue) for issue in issues.split()):
+        raise exceptions.AnswerRequiredError("Enter valid Jira issue keys (e.g., JRA-123).")
+
+    return issues
+
+
 class ConventionalCommitsAnswers(TypedDict):
+    jira_issues: str
+    jira_workflow: str
+    jira_time: str
+    jira_comment: str
     prefix: str
     scope: str
     subject: str
@@ -41,6 +56,30 @@ class ConventionalCommitsJiraOptionalCz(BaseCommitizen):
 
     def questions(self) -> list[CzQuestion]:
         return [
+            {
+                "type": "input",
+                "name": "jira_issues",
+                "message": "Jira Issue ID(s) separated by spaces (optional):\n",
+                "filter": _parse_jira_issues,
+            },
+            {
+                "type": "input",
+                "name": "jira_workflow",
+                "message": "Workflow command (testing, closed, etc.) (optional):\n",
+                "filter": lambda x: "#" + x.strip().replace(" ", "-") if x else "",
+            },
+            {
+                "type": "input",
+                "name": "jira_time",
+                "message": "Time spent (i.e. 3h 15m) (optional):\n",
+                "filter": lambda x: "#time " + x if x else "",
+            },
+            {
+                "type": "input",
+                "name": "jira_comment",
+                "message": "Jira comment (optional):\n",
+                "filter": lambda x: "#comment " + x if x else "",
+            },
             {
                 "type": "list",
                 "name": "prefix",
@@ -142,6 +181,11 @@ class ConventionalCommitsJiraOptionalCz(BaseCommitizen):
         ]
 
     def message(self, answers: ConventionalCommitsAnswers) -> str:  # type: ignore[override]
+        jira_issues = answers["jira_issues"]
+        jira_workflow = answers["jira_workflow"]
+        jira_time = answers["jira_time"]
+        jira_comment = answers["jira_comment"]
+
         prefix = answers["prefix"]
         scope = answers["scope"]
         subject = answers["subject"]
@@ -151,15 +195,26 @@ class ConventionalCommitsJiraOptionalCz(BaseCommitizen):
 
         formatted_scope = f"({scope})" if scope else ""
         title = f"{prefix}{formatted_scope}"
-        if is_breaking_change:
-            if self.config.settings.get("breaking_change_exclamation_in_title", False):
-                title = f"{title}!"
-            footer = f"BREAKING CHANGE: {footer}"
+
+        if is_breaking_change and self.config.settings.get("breaking_change_exclamation_in_title", False):
+            title = f"{title}!"
+
+        if jira_issues:
+            title = f"{title}: {jira_issues} {subject}"
+        else:
+            title = f"{title}: {subject}"
 
         formatted_body = f"\n\n{body}" if body else ""
-        formatted_footter = f"\n\n{footer}" if footer else ""
 
-        return f"{title}: {subject}{formatted_body}{formatted_footter}"
+        jira_parts = [x for x in [jira_workflow, jira_time, jira_comment] if x]
+        formatted_jira = f"\n\n{' '.join(jira_parts)}" if jira_parts else ""
+
+        if is_breaking_change:
+            footer = f"BREAKING CHANGE: {footer}"
+
+        formatted_footer = f"\n\n{footer}" if footer else ""
+
+        return f"{title}{formatted_body}{formatted_jira}{formatted_footer}"
 
     def example(self) -> str:
         return (
@@ -171,7 +226,14 @@ class ConventionalCommitsJiraOptionalCz(BaseCommitizen):
         )
 
     def schema(self) -> str:
-        return "<type>(<scope>): <subject>\n" "<BLANK LINE>\n" "<body>\n" "<BLANK LINE>\n" "(BREAKING CHANGE: )<footer>"
+        return (
+            "<type>(<scope>): <jira_issues> <subject>\n"
+            "<BLANK LINE>\n"
+            "<body>\n"
+            "<BLANK LINE>\n"
+            "<jira_workflow> <jira_time> <jira_comment>\n"
+            "(BREAKING CHANGE: )<footer>"
+        )
 
     def schema_pattern(self) -> str:
         change_types = (
@@ -188,18 +250,27 @@ class ConventionalCommitsJiraOptionalCz(BaseCommitizen):
             "style",
             "test",
         )
+
+        jira_issues_pattern = r"([A-Z]{2,}-\d+( [A-Z]{2,}-\d+)*)?"
+        jira_metadata_pattern = r"(?:\s#\w+(?: [^\s#]+)?)?"
+
         return (
-            r"(?s)"  # To explicitly make . match new line
+            r"(?s)"  # Explicitly make . match new line
             r"(" + "|".join(change_types) + r")"  # type
-            r"(\(\S+\))?"  # scope
+            r"(\(\S+\))?"  # Optional scope
             r"!?"
             r": "
-            r"([^\n\r]+)"  # subject
-            r"((\n\n.*)|(\s*))?$"
+            + jira_issues_pattern  # Jira issues
+            + r"\s*([^\n\r]+)?"  # Subject
+            + r"(\n\n.*)?"  # Optional body
+            + r"("
+            + jira_metadata_pattern
+            + r")*"  # Optional Jira metadata repeated
+            + r"$"
         )
 
     def info(self) -> str:
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        filepath = os.path.join(dir_path, "conventional_commits_info.txt")
+        filepath = os.path.join(dir_path, "cz_conventional_commmits_jira_optional_info.txt")
         with open(filepath, encoding=self.config.settings["encoding"]) as f:  # type: ignore
             return f.read()
