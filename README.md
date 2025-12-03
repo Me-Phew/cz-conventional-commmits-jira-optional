6c455f3aac9ecb002edbcfe765dcb7cc6b440040
Mateusz Basiaga <basmateusz@wp.pl>
2025-12-03 14:27:59 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cz_conventional_commmits_jira_optional
+
+## Usage
+
+## Author
+
+Mateusz Basiaga (basmateusz@wp.pl)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cz-conventional-commmits-jira-optional!")
+
+
+if __name__ == "__main__":
+    main()
